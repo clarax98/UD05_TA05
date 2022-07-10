e46c4a4acd18b3c65281b5664f6a00e824ce8d8d
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 15:56:39 +0200

    TA05_02

---
+++ UD05/src/TA05_02/mainApp.java
@@ -0,0 +1,11 @@
+package TA05_02;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Clara";
+		System.out.println("Bienvenidx "+name);
+	}
+
+}
