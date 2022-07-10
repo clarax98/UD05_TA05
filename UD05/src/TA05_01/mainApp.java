0f7a52d03dd67d634cf7ee083c4c471c774cb4fb
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 15:54:20 +0200

    TA05_01

---
+++ UD05/src/TA05_01/mainApp.java
@@ -0,0 +1,17 @@
+package TA05_01;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 155, y = 15;
+		if(x<y) {
+			System.out.println("X < Y");
+		} else if(x>y) {
+			System.out.println("X > Y");
+		} else {
+			System.out.println("X = Y");
+		}
+	}
+
+}
