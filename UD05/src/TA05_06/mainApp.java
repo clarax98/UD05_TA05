5d75efa1aab8f29bd5bc1450f414aecd7b4fa5ab
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 16:40:09 +0200

    TA05_06

--- UD05/src/TA05_06/mainApp.java
+++ UD05/src/TA05_06/mainApp.java
@@ -1,13 +1,19 @@
 package TA05_06;
 
+import javax.swing.JOptionPane;
+
 public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int c = 1;
-		while (c <= 100) {
-			System.out.println(c++);
-		}
+		String precio = "";
+		final double IVA = 1.21;
+		
+		do {
+			precio = JOptionPane.showInputDialog("Introduce el precio sin IVA");
+		} while (precio.isEmpty());
+	
+		JOptionPane.showMessageDialog(null, "El precio con el IVA es: " + Double.parseDouble(precio) * IVA);
 	}
 
 }
