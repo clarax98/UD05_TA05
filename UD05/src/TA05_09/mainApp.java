64b9337128eddef083135f5394e7e3882a15d1f0
Clara Ogalla <claraogalla@gmail.com>
2022-07-10 16:46:06 +0200

    TA05_09

---
+++ UD05/src/TA05_09/mainApp.java
@@ -0,0 +1,14 @@
+package TA05_09;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 100; i++) {
+			if((i%2 == 0)||(i%3 == 0)) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
